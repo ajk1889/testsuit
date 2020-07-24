09124538032102e35307921d73c8cbb2ecc7a800
ajk1889 <ajk1889@gmail.com>
2020-07-24 21:04:25 +0530

    Added additional functions to Server.h

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include <unistd.h>
 #include "server/Server.h"
 
+thread *asyncTest() {
+    return new thread([] {
+        for (int i = 0; i < 120; ++i) {
+            usleep(30000);
+            std::cout << i << std::endl;
+        }
+    });
+}
+
 int main() {
-    Server::test();
+    auto thread = asyncTest();
+    usleep(3000000);
+    delete thread;
     return 0;
 }
