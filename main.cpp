e1fcc83b1224889c1b98b9af9455f1f5f14049f7
Amaljith <ajk1889@gmail.com>
2020-09-10 20:01:39 +0530

    Added JSON parser library

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
-#include "server/Server.h"
-#include "implementations/Process.h"
-
-void runServer() {
-    Server server;
-    server.start();
-    char i = 0;
-    std::cin >> i;
-    server.stop();
-}
+#include "lib/json.hpp"
+//#include "server/Server.h"
+//#include "implementations/Process.h"
+//
+//void runServer() {
+//    Server server;
+//    server.start();
+//    char i = 0;
+//    std::cin >> i;
+//    server.stop();
+//}
+using json = nlohmann::json;
 int main() {
-    Process::test();
+    auto data = json::parse(R"({"name":"json","items":[1,2,3],"another":{"a":"b"}})");
+    data["abc"] = "asd";
+    std::cout << data << std::endl;
     return 0;
 }
