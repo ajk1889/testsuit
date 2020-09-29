5b8c1305ef663e756b28e1b5d6f9145c95b76da2
Amaljith <ajk1889@gmail.com>
2020-09-29 08:52:38 +0530

    File upload module partially complete

--- modules/upload.py
+++ modules/upload.py
@@ -1,4 +1,7 @@
 import json
+import os
+import random
+import shutil
 
 
 def echo(code, html):
@@ -12,8 +15,60 @@ def echo(code, html):
     print(html)
 
 
+def find_file_name(headers):
+    disposition = headers["Content-Disposition"][0]
+    if "filename=" in disposition:
+        name = disposition.split("filename=")[1]
+        if name[0] == '"' or name[0] == "'":
+            name = name[1:]
+        if name[-1] == '"' or name[-1] == "'":
+            name = name[:-1]
+        return name
+    else:
+        return str(random.randint(10 ** 9, 10 ** 10))
+
+
+def get_output_path(relative_path):
+    if os.path.exists(file_path) and os.path.isdir(file_path):
+        return os.path.join(file_path, relative_path)
+    elif file_path[-1] == '/':
+        os.makedirs(file_path)
+        return os.path.join(file_path, relative_path)
+    else:
+        return file_path
+
+
+def save_file(file):
+    if file:
+        file_name = find_file_name(file["headers"])
+        if file_name == "":
+            return False
+        else:
+            file_data = file["data"]["data"]
+            if data == "":
+                return False
+            if file["data"]["isFile"]:
+                shutil.move(file_data, get_output_path(file_name))
+            else:
+                with open(get_output_path(file_name), 'wb') as op:
+                    op.write(file_data)
+            return True
+    else:
+        return False
+
+
 data = json.loads(input().strip())
-success_msg = "<h3>Upload successful</h3>" if data["POST"] else ""
+post = data["POST"]
+if post:
+    success_msg = "<h3>Upload successful</h3>"
+    if "path" in post and post["path"]:
+        file_path = post["path"][0]["data"]["data"]
+    else:
+        file_path = os.getcwd()
+    if "file" in post:
+        save_file(post["file"][0])
+else:
+    success_msg = ""
 response = f'''
 <html>
     <title>Upload files</title>
