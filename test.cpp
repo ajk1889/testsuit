18c1b284177ef9996ab9453778d84db03a16df3d
Amaljith <ajk1889@gmail.com>
2020-10-08 20:01:31 +0530

    Removed generator dependencies

--- test.cpp
+++ test.cpp
@@ -1,6 +1,5 @@
 #include "implementations/utils/Process.h"
 #include "server/Server.h"
-#include "implementations/Generator.h"
 
 void Process::test() {
     Process process = {"python3", "-c", "print(int(input())**237)"};
@@ -34,32 +33,4 @@ void Server::test() {
     thread client(connectToServer);
     server.join();
     client.join();
-}
-
-void visualize() {
-    char memory[51]{};
-    memory[50] = '\0';
-    for (int i = 0; i < 5; ++i) {
-        Generator::get(i * 50, (i + 1) * 50, memory);
-        std::cout << memory << std::endl;
-    }
-}
-
-void performanceTest() {
-    constexpr auto bfr = 2 * 1024UL * 1024UL;
-    constexpr auto size = 2 * 1024UL * 1024UL * 1024UL;
-
-    auto *memory = new char[bfr];
-    auto read = 0UL;
-
-    auto t_start = std::chrono::high_resolution_clock::now();
-    Generator generator(0);
-    while (read < size) {
-        generator.read(memory, bfr);
-        read += bfr;
-    }
-    auto t_end = std::chrono::high_resolution_clock::now();
-    delete[] memory;
-
-    std::cout << std::chrono::duration<double, std::milli>(t_end - t_start).count() << std::endl;
 }
